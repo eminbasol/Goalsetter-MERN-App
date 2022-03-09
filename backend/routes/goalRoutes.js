f6090a50b181a00a353a43a2809c74e7903ecb23
eminbasol <eminbasol08@gmail.com>
2022-03-09 15:07:24 +0300

    User authentication and authorization

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
+const { protect } = require('../middleware/authMiddleware')
+
 /* 
 Same Route: router.get('/', getGoals) - router.post('/', setGoal)
 Same Route: router.put('/:id', updateGoal) - router.delete('/:id', deleteGoal) 
 */
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').put(updateGoal).delete(deleteGoal)
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoal)
 
 module.exports = router
